328790e149d5fd04ad227132f86e8dc1708e443f
AgvanGrigoryan <agvangrigoryan2003@gmail.com>
2023-04-21 00:24:06 +0400

    Initial commit

---
+++ now_or_never/task/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from task.models import Folder, Task
+
+
+@admin.register(Folder)
+class FolderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'user', 'slug')
+    list_display_links = ('id', 'name', 'slug')
+    list_filter = ('user',)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'completionDate', 'author', 'folder', 'isDone')
+    list_display_links = ('id', 'title')
+    list_filter = ('author', 'folder', 'isDone')
---
+++ now_or_never/task/models.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Folder(models.Model):
+    name = models.CharField(max_length=15, verbose_name='Название Папки')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Владелец')
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Папка'
+        verbose_name_plural = 'Папки'
+
+
+class Task(models.Model):
+    author = models.ForeignKey(User, related_name='tasks', on_delete=models.CASCADE, verbose_name='Автор' )
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    publicationDate = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    completionDate = models.DateTimeField(verbose_name='Дата выполнения')
+    folder = models.ForeignKey(Folder, null=True, blank=True, on_delete=models.SET_NULL, verbose_name='Папка')
+    color = models.CharField(null=False, max_length=7, verbose_name='Цвет', help_text='Указывать')
+    isDone = models.BooleanField(default=False, verbose_name='Выполнен')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+        db_table_comment = "User's tasks"
