ad9ea59530657d3d017d951324efbb3dd104c763
AgvanGrigoryan <agvangrigoryan2003@gmail.com>
2023-04-30 17:02:41 +0400

    select first preset_color as default task_color on todo create page.

--- now_or_never/static/scripts/Task.js
+++ now_or_never/static/scripts/Task.js
@@ -1,6 +1,10 @@
 let preset_color = document.querySelectorAll('.preset_color');
 let r = document.querySelector(':root');
 
+let add_todo_form_container = document.querySelector('.add-toDo');
+if(add_todo_form_container) {
+    add_todo_form_container.querySelector('.colors').firstElementChild.querySelector('.color_input').setAttribute('checked', 'checked');
+}
 
 preset_color.forEach(e => {
     let color_input = e.querySelector('.color_input')
@@ -14,3 +18,4 @@ preset_color.forEach(e => {
         r.style.setProperty('--task-color', task_color);
     });
 });
+
